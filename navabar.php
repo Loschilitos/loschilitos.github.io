652db558577d725fb244266b6a62027f8067e58f
Loschilitos <mitchell.munoz127@gmail.com>
2024-09-08 18:04:30 -0600

    Update and rename navabar.html to navabar.php

---
+++ navabar.php
@@ -0,0 +1,11 @@
+<!-- navbar.php -->
+<nav>
+    <div class="container">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="mission.php">Mission Trips</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
